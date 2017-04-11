76ab65b905b6979b582c05f1094ce565dd032347
Ram <erroju.ram@gmail.com>
2017-04-11 17:53:42 +0530

    intial code

--- app/src/main/java/com/ram/contacts/MainActivity.java
+++ app/src/main/java/com/ram/contacts/MainActivity.java
@@ -1,13 +1,136 @@
 package com.ram.contacts;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.ram.contacts.adapters.ContactsAdapter;
+import com.ram.contacts.model.Contact;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mContactsRecyclerView;
+    private ContactsAdapter mContactsAdapter;
+    private List<Contact> mContactList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContactsRecyclerView = (RecyclerView) findViewById(R.id.contact_recycler_view);
+        mContactsAdapter = new ContactsAdapter(mContactList);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mContactsRecyclerView.setLayoutManager(mLayoutManager);
+        mContactsRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mContactsRecyclerView.setAdapter(mContactsAdapter);
+
+
+        fetchContactData();
+    }
+
+    private void fetchContactData() {
+
+       /* Contact mContact = new Contact("Mad Max: Fury Road", "Action & Adventure");
+        mContactList.add(mContact);
+
+        mContact = new Contact("Inside Out", "Animation, Kids & Family");
+        mContactList.add(mContact);
+
+        mContact = new Contact("Star Wars: Episode VII - The Force Awakens", "Action");
+        mContactList.add(mContact);
+*/
+
+        new AsyncTask() {
+            ProgressDialog pDialog = new ProgressDialog(MainActivity.this);
+            StringBuilder stringResponse = new StringBuilder();
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                pDialog.setIndeterminate(true);
+                pDialog.setMessage("Please wait... data is being downloaded...");
+                pDialog.show();
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                super.onPostExecute(o);
+                if (stringResponse.length() > 0) {
+                    try {
+                        JSONObject jObj = new JSONObject(stringResponse.toString());
+                        JSONArray contacts = jObj.getJSONArray("contacts");
+
+                        for (int i = 0; i < contacts.length(); i++) {
+                            String name = contacts.getJSONObject(i).get("name").toString();
+                            String email = contacts.getJSONObject(i).get("email").toString();
+                            Contact mContact = new Contact(name, email);
+                            mContactList.add(mContact);
+                        }
+                    } catch (Exception e) {
+
+                    }
+
+                }
+                mContactsAdapter.notifyDataSetChanged();
+                pDialog.dismiss();
+            }
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                stringResponse.setLength(0);
+                URL url;
+                BufferedReader reader = null;
+                HttpURLConnection urlConnection = null;
+                try {
+                    url = new URL("http://api.androidhive.info/contacts/");
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+
+                    reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        // Append server response in string
+                        stringResponse.append(line + "");
+                    }
+
+                    Log.v("Test", stringResponse + "");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+                return null;
+            }
+
+
+        }.execute();
+
+
     }
 }
---
+++ app/src/main/java/com/ram/contacts/adapters/ContactsAdapter.java
@@ -0,0 +1,64 @@
+package com.ram.contacts.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ram.contacts.R;
+import com.ram.contacts.model.Contact;
+
+import java.util.List;
+
+/**
+ * Created by Vijay on 11/04/17.
+ */
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.MyViewHolder> {
+
+    private List<Contact> mContactsList;
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+
+    public ContactsAdapter(List<Contact> mContactsList) {
+        this.mContactsList = mContactsList;
+    }
+
+    /*public void setData(List<Contact> mContactsList) {
+        this.mContactsList = mContactsList;
+        notifyDataSetChanged();
+    }*/
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        Contact contact = mContactsList.get(position);
+        holder.name.setText(contact.getName());
+        holder.email.setText(contact.getEmail());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContactsList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, email;
+
+        public MyViewHolder(View view) {
+            super(view);
+            name = (TextView) view.findViewById(R.id.name);
+            email = (TextView) view.findViewById(R.id.email);
+
+        }
+    }
+}
